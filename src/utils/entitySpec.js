a63963eee1482d687eb0eb4a11004d51d46782a5
Jeff McAffer <jmcaffer@microsoft.com>
2018-02-14 17:44:13 -0800

    normalize coordinate casing

--- src/utils/entitySpec.js
+++ src/utils/entitySpec.js
@@ -1,6 +1,25 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
+const NAMESPACE = 0x4;
+const NAME = 0x2;
+const REVISION = 0x1;
+const NONE = 0;
+
+const toLowerCaseMap = {
+  github: NAMESPACE | NAME,
+  npmjs: NONE,
+  mavencentral: NONE,
+  mavencentralsource: NONE
+}
+
+function normalize(value, provider, property) {
+  if (!value)
+    return value;
+  const mask = toLowerCaseMap[provider] || 0;
+  return (mask & property) ? value.toLowerCase() : value;
+}
+
 export default class EntitySpec {
 
   static fromPath(path) {
@@ -16,18 +35,18 @@ export default class EntitySpec {
   }
 
   constructor(type, provider, namespace, name, revision = null, pr = null) {
-    this.type = type
-    this.provider = provider
-    this.namespace = namespace === '-' ? null : namespace
-    this.name = name
-    this.revision = revision
+    this.type = type.toLowerCase()
+    this.provider = provider.toLowerCase()
+    this.namespace = namespace === '-' ? null : normalize(namespace, this.provider, NAMESPACE);
+    this.name = normalize(name, this.provider, NAME);
+    this.revision = normalize(revision, this.provider, REVISION);
     this.pr = pr
   }
 
   toUrl() {
     return `cd:/${this.toUrlPath()}`
-  }  
-  
+  }
+
   toUrlPath() {
     const revisionPart = this.revision ? `/${this.revision}` : ''
     const prPart = this.pr ? `/pr/${this.pr}` : ''
