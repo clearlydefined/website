fae5fb09d77c3b71c1e5d5aa321c5f121bca65de
William Bartholomew <wdb@willbar.com>
2018-03-02 15:52:19 -0800

    Adding Google Analytics for client telemetry

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import ReactDOM from 'react-dom'
 import './styles/index.css'
 import registerServiceWorker from './registerServiceWorker'
 import { RehydrationProvider } from './components'
+import ReactGA from 'react-ga'
+ReactGA.initialize(process.env['REACT_APP_GA_TRACKINGID'])
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 ReactDOM.render(<RehydrationProvider />, document.getElementById('root'))
 registerServiceWorker()
