8708dcc516ad488be4ab6a2ddc46bf368d94bff1
Jeff McAffer <jmcaffer@microsoft.com>
2018-03-02 23:31:51 -0800

    add subbranding to landing page

--- src/components/Clearly.js
+++ src/components/Clearly.js
@@ -1,11 +1,15 @@
 // Copyright (c) Microsoft Corporation and others. Licensed under the MIT license.
 // SPDX-License-Identifier: MIT
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-export const primaryColor = { color: '#0064b5' };
-export const secondaryColor = { color: '#60b360' };
+export const primaryColor = { color: '#0064b5' }
+export const secondaryColor = { color: '#60b360' }
+export const licensedColor = { color: '#38a038'}
+export const describedColor = { color: '#00b294'}
+export const secureColor = { color: '#409ae1'}
+export const definedColor = primaryColor
 
 export default class Clearly extends Component {
   static propTypes = {
