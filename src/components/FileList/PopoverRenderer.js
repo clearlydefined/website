7e916cc1f44e1cda02a34e630d9a2ab14b05a56a
Simone Torrisi <storrisi@gmail.com>
2018-07-25 18:17:20 +0200

    Fixed Popover Behaviour

--- src/components/FileList/PopoverRenderer.js
+++ src/components/FileList/PopoverRenderer.js
@@ -5,7 +5,7 @@ import { Popover } from 'react-bootstrap'
 const PopoverComponent = (props) => {
   return (<Popover id="popover-positioned-left" title={props.title} {...props}>
     <div style={{ maxHeight: '200px', overflowY: 'auto', overflowX: 'hidden', width: '100%' }}>
-      {props.values && isArray(props.value) ? props.values.map(item => <p key={item}>{item}</p>) : <p>{props.values}</p>}</div>
+      {props.values && isArray(props.values) ? props.values.map(item => <p key={item}>{item}</p>) : <p>{props.values}</p>}</div>
   </Popover>)
 }
 
