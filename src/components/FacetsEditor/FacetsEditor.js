deb2c70b9bede2303d16c5d54f348617a98ff3ac
Gianfranco Palumbo <gianpa@gmail.com>
2018-08-10 17:07:42 +0300

    FacetsEditor: use Grid, Col and Grid from bootstrap

--- src/components/FacetsEditor/FacetsEditor.js
+++ src/components/FacetsEditor/FacetsEditor.js
@@ -3,6 +3,7 @@
 
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Col, Grid, Row } from 'react-bootstrap'
 import InlineEditor from '../InlineEditor'
 
 // Hardcoded, maybe it should be kept somewhere
@@ -26,30 +27,28 @@ class FacetsEditor extends Component {
     const { onChange, getValue, classIfDifferent } = this.props
 
     return (
-      <div className="facetsEditor">
-        <h2>FACETS</h2>
-        <div>
-          {facets.map(item => (
-            <div key={item} className="item">
-              <div className="column">
-                <span>{item}</span>
-              </div>
-              <div className="column">
-                <InlineEditor
-                  extraClass={classIfDifferent(`described.facets.${item}`, 'facets__isEdited')}
-                  readOnly={false}
-                  type="text"
-                  initialValue={''}
-                  value={getValue(`described.facets.${item}`)}
-                  onChange={value => onChange(`described.facets.${item}`, value)}
-                  validator={value => true}
-                  placeholder={`${item} facet`}
-                />
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+      <Grid className="no-gutters">
+      <h2>FACETS</h2>
+      {facets.map(item => (
+        <Row key={item}>
+          <Col md={1}>
+            <span>{item}</span>
+          </Col>
+          <Col md={11}>
+            <InlineEditor
+              extraClass={classIfDifferent(`described.facets.${item}`, 'facets__isEdited')}
+              readOnly={false}
+              type="text"
+              initialValue={''}
+              value={getValue(`described.facets.${item}`)}
+              onChange={value => onChange(`described.facets.${item}`, value)}
+              validator={value => true}
+              placeholder={`${item} facet`}
+            />
+          </Col>
+        </Row>
+      ))}
+      </Grid>
     )
   }
 }
