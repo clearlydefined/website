c6d848bcc3149b0f79faef85d38b87403a71fa83
Simone Torrisi <storrisi@gmail.com>
2018-10-17 18:00:12 +0200

    Removed unused components

--- src/components/Renderers/LabelRenderer.js
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation and others. Licensed under the MIT license.
-// SPDX-License-Identifier: MIT
-import React from 'react'
-
-const LabelRenderer = ({ text }) => <b>{text}</b>
-export default LabelRenderer
