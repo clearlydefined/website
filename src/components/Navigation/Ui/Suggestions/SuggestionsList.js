a58f3d1ef9571614b0689ae6cee8a741ab6d0d93
Simone Torrisi <storrisi@gmail.com>
2018-11-17 19:11:11 +0100

    Show suggestions list

--- src/components/Navigation/Ui/Suggestions/SuggestionsList.js
+++ src/components/Navigation/Ui/Suggestions/SuggestionsList.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react'
 
 class SuggestionsList extends Component {
   render() {
-    return <div>{this.props.items}</div>
+    return (
+      <div>
+        {this.props.items.map(item => (
+          <p key={item.version}>{item.value}</p>
+        ))}
+      </div>
+    )
   }
 }
 
