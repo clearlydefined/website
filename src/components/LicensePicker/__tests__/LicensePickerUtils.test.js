3ea1c6a018c12e127697c72cc4d223df71239fee
Simone Torrisi <storrisi@gmail.com>
2018-12-10 14:44:56 +0100

    LicensePickerUtils tests

--- src/components/LicensePicker/__tests__/LicensePickerUtils.test.js
+++ src/components/LicensePicker/__tests__/LicensePickerUtils.test.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import LicensePicker from '../LicensePicker'
+import LicensePickerUtils from '../utils'
+
+const testRules = [
+  { license: 'MIT', operator: 'AND', laterVersions: false },
+  { license: 'Apache-2.0', operator: '', laterVersions: false }
+]
 
 describe('LicensePicker', () => {
   it('renders without crashing', () => {
-    shallow(<LicensePicker />)
+    const licenseString = LicensePickerUtils.getLicenseString(testRules)
+    expect(licenseString).toBe('MIT AND Apache-2.0')
   })
 })
