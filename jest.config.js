ebac1dad9986f9b211eb89adb8bc6b6c411b9305
Simone Torrisi <storrisi@gmail.com>
2019-01-22 19:34:00 +0100

    Unit and Integration test separation configuration

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  verbose: true,
+  moduleFileExtensions: ['js', 'jsx', 'json'],
+  moduleDirectories: ['node_modules'],
+  transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/config/jest/assetsTransformer.js',
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js'
+  }
+}
