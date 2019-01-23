4e17baedcafe5d3b47b52c0194d26f807a3da0c7
Simone Torrisi <storrisi@gmail.com>
2019-01-23 17:48:31 +0100

    added revert functionality to ModalEditor

--- src/components/ModalEditor.js
+++ src/components/ModalEditor.js
@@ -8,12 +8,19 @@ import 'react-bootstrap-typeahead/css/Typeahead.css'
 
 export default class ModalEditor extends React.Component {
   static propTypes = {
+    field: PropTypes.string.isRequired,
     readOnly: PropTypes.bool,
     initialValue: PropTypes.string,
     extraClass: PropTypes.string,
     value: PropTypes.string,
     onChange: PropTypes.func.isRequired,
-    placeholder: PropTypes.string.isRequired
+    placeholder: PropTypes.string.isRequired,
+    onRevert: PropTypes.func,
+    revertable: PropTypes.bool
+  }
+
+  static defaultProps = {
+    revertable: true
   }
 
   state = { editing: false }
@@ -25,7 +32,7 @@ export default class ModalEditor extends React.Component {
   }
 
   renderValue() {
-    const { value, initialValue, placeholder, extraClass, readOnly, onClick, editor, onChange } = this.props
+    const { value, initialValue, placeholder, extraClass, field, readOnly, onClick, editor, onChange } = this.props
     const { editing } = this.state
     const changed = initialValue !== value
 
@@ -33,7 +40,7 @@ export default class ModalEditor extends React.Component {
       <span>
         <span
           title={value}
-          className={`editable-field ${extraClass} ${value ? (changed ? 'bg-info' : '') : 'placeholder-text'}`}
+          className={`editable-field ${extraClass} ${field} ${value ? (changed ? 'bg-info' : '') : 'placeholder-text'}`}
           onClick={() => (readOnly ? null : this.setState({ editing: true }, () => onClick && onClick()))}
         >
           {value || placeholder}
@@ -53,7 +60,8 @@ export default class ModalEditor extends React.Component {
   }
 
   render() {
-    const { onClick, readOnly } = this.props
+    const { onClick, onRevert, revertable, readOnly, initialValue, value } = this.props
+    const changed = initialValue !== value
     return (
       <span className="list-singleLine">
         {!readOnly && (
@@ -62,6 +70,13 @@ export default class ModalEditor extends React.Component {
             onClick={() => this.setState({ editing: true }, () => onClick && onClick())}
           />
         )}
+        {!readOnly &&
+          revertable && (
+            <i
+              className={`fas fa-undo editable-marker ${!changed && 'fa-disabled'}`}
+              onClick={() => onRevert && changed && onRevert()}
+            />
+          )}
         {this.renderValue()}
       </span>
     )
