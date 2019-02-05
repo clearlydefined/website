495ae1559c2efa609096b61f9453f24fa77898ea
Simone Torrisi <storrisi@gmail.com>
2019-02-05 18:12:04 +0100

    Added test to validate a license using spdx-expression-validate

--- src/components/LicensePicker/__tests__/LicensePickerUtils.test.unit.js
+++ src/components/LicensePicker/__tests__/LicensePickerUtils.test.unit.js
@@ -1,4 +1,6 @@
 import LicensePickerUtils from '../utils'
+import valid from 'spdx-expression-validate'
+import parse from 'spdx-expression-parse'
 
 const testRules = {
   left: { license: 'Apache-2.0' },
@@ -14,5 +16,19 @@ describe('LicensePickerUtils', () => {
   it('parse a definition with no license', () => {
     const licenseString = LicensePickerUtils.parseLicense('NOASSERTION')
     expect(licenseString).toEqual({ license: 'NOASSERTION' })
+  })
+  it('validate a license', () => {
+    const license = 'MIT-0'
+    const licenseParsed = parse(license)
+    const licenseValid = valid(license)
+    expect(licenseParsed).toEqual({ license })
+    expect(licenseValid).toBe(true)
+  })
+  it('validate a license', () => {
+    const license = 'MIT-CMU'
+    const licenseParsed = parse(license)
+    const licenseValid = valid(license)
+    expect(licenseParsed).toEqual({ license })
+    expect(licenseValid).toBe(true)
   })
 })
