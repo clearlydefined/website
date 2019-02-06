0a6ee428e77f8aafa4da7ed9645f5d524ebd6142
Dan Butvinik <dabutvin@microsoft.com>
2019-02-05 16:24:33 -0800

    setup status page

---
+++ src/components/Navigation/Pages/PageStatus/PageStatus.js
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation and others. Licensed under the MIT license.
+// SPDX-License-Identifier: MIT
+
+import React, { Component } from 'react'
+
+export default class PageStatus extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Hello!</h2>
+      </div>
+    )
+  }
+}
