4bb5854d1e772a4b6645c19f50d6c9ba183c70bd
Dan Butvinik <dabutvin@microsoft.com>
2019-02-15 15:53:48 -0800

    allow __HOST__ to be read from env

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'jest-puppeteer',
   testRegex: 'e2e/tests/.*\\.js$',
   globals: {
-    __HOST__: 'http://localhost:3333'
+    __HOST__: process.env.TEST_HOST || 'http://localhost:3333'
   },
   transformIgnorePatterns: ['<rootDir>/node_modules/']
 }
