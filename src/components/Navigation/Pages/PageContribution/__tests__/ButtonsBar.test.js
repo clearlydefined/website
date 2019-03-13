f01b83070859a866430adc01297b3a6fde69076e
Gianfranco Palumbo <gianpa@gmail.com>
2019-03-13 12:25:38 +0200

    fix integration test. 2 tests were removed from the ButtonsBar PageContribution

--- src/components/Navigation/Pages/PageContribution/__tests__/ButtonsBar.test.js
+++ src/components/Navigation/Pages/PageContribution/__tests__/ButtonsBar.test.js
@@ -10,30 +10,4 @@ describe('ButtonsBar', () => {
   it('renders without crashing', () => {
     shallow(<ButtonsBar />)
   })
-
-  it("checks if buttons are enabled when there aren't changes", async () => {
-    const wrapper = await shallow(
-      <ButtonsBar
-        components={{
-          list: []
-        }}
-        hasChanges={false}
-      />
-    )
-    const doneButton = wrapper.find({ children: 'Done' })
-    expect(doneButton.exists()).toBeTruthy()
-    expect(doneButton.prop('disabled')).toBe(false)
-    const collapseButton = wrapper.find({ children: 'Collapse All' })
-    expect(collapseButton.exists()).toBeTruthy()
-    expect(collapseButton.prop('disabled')).toBe(false)
-  })
-  it('checks if buttons are disabled when there are changes', async () => {
-    const wrapper = await shallow(<ButtonsBar components={components} hasChanges={true} />)
-    const doneButton = wrapper.find({ children: 'Done' })
-    expect(doneButton.exists()).toBeTruthy()
-    expect(doneButton.prop('disabled')).toBe(true)
-    const collapseButton = wrapper.find({ children: 'Collapse All' })
-    expect(collapseButton.exists()).toBeTruthy()
-    expect(collapseButton.prop('disabled')).toBe(true)
-  })
 })
