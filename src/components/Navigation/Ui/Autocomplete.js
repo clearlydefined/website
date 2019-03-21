eaa9c4448c547dfabe1dddca6c1c46fada49a5f8
Simone Torrisi <storrisi@gmail.com>
2019-03-21 17:37:12 +0100

    Added highlighter

--- src/components/Navigation/Ui/Autocomplete.js
+++ src/components/Navigation/Ui/Autocomplete.js
@@ -3,7 +3,7 @@
 
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { Typeahead, Menu, menuItemContainer } from 'react-bootstrap-typeahead'
+import { Typeahead, Menu, menuItemContainer, Highlighter } from 'react-bootstrap-typeahead'
 import { MenuItem } from 'react-bootstrap'
 import get from 'lodash/get'
 import 'react-bootstrap-typeahead/css/Typeahead.css'
@@ -11,27 +11,25 @@ import 'react-bootstrap-typeahead/css/Typeahead.css'
 const TypeaheadMenuItem = menuItemContainer(MenuItem)
 
 export default class Autocomplete extends Component {
-  render() {
-    return (
-      <Typeahead
-        {...this.props}
-        renderMenu={(results, menuProps) =>
-          ReactDOM.createPortal(
-            <Menu {...menuProps}>
-              {results.map((result, index) => (
-                <TypeaheadMenuItem
-                  key={menuProps.labelKey && get(result, menuProps.labelKey) ? get(result, menuProps.labelKey) : result}
-                  option={result}
-                  position={index}
-                >
-                  {menuProps.labelKey && get(result, menuProps.labelKey) ? get(result, menuProps.labelKey) : result}
-                </TypeaheadMenuItem>
-              ))}
-            </Menu>,
-            document.body
-          )
-        }
-      />
+  renderMenu = (results, menuProps) =>
+    ReactDOM.createPortal(
+      <Menu {...menuProps}>
+        {results.map((result, index) => (
+          <TypeaheadMenuItem
+            key={menuProps.labelKey && get(result, menuProps.labelKey) ? get(result, menuProps.labelKey) : result}
+            option={result}
+            position={index}
+          >
+            <Highlighter search={menuProps.text}>
+              {menuProps.labelKey && get(result, menuProps.labelKey) ? get(result, menuProps.labelKey) : result}
+            </Highlighter>
+          </TypeaheadMenuItem>
+        ))}
+      </Menu>,
+      document.body
     )
+
+  render() {
+    return <Typeahead {...this.props} renderMenu={this.renderMenu} />
   }
 }
