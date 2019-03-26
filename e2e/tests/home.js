b0628ba8d16fcb2b634efe5aef15018e6ad83fb4
Gianfranco Palumbo <gianpa@gmail.com>
2019-03-26 10:49:21 +0200

    home: no need to use async await when returning one Promise.
    see: https://jestjs.io/docs/en/asynchronous#promises

--- e2e/tests/home.js
+++ e2e/tests/home.js
@@ -2,23 +2,23 @@
 // SPDX-License-Identifier: MIT
 
 describe('Home page', () => {
-  beforeAll(async () => {
-    await page.goto(`${__HOST__}/about`)
+  beforeAll(() => {
+    return page.goto(`${__HOST__}/about`)
   })
 
-  it('should display "ClearlyDefined" text on page', async () => {
-    await expect(page).toMatch('ClearlyDefined')
+  it('should display "ClearlyDefined" text on page', () => {
+    return expect(page).toMatch('ClearlyDefined')
   })
 
   describe('Home page sections', () => {
-    it('should have a "ClearlyDescribed" section', async () => {
-      await expect(page).toMatch('ClearlyDescribed')
+    it('should have a "ClearlyDescribed" section', () => {
+      return expect(page).toMatch('ClearlyDescribed')
     })
-    it('should have a "ClearlyLicensed" section', async () => {
-      await expect(page).toMatch('ClearlyLicensed')
+    it('should have a "ClearlyLicensed" section', () => {
+      return expect(page).toMatch('ClearlyLicensed')
     })
-    it('should have a "ClearlySecure" section', async () => {
-      await expect(page).toMatch('ClearlySecure')
+    it('should have a "ClearlySecure" section', () => {
+      return expect(page).toMatch('ClearlySecure')
     })
   })
 })
