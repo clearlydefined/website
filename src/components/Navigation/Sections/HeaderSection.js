677acb7f85e370151fa4c49711da68ad07c83be1
Simone Torrisi <storrisi@gmail.com>
2019-04-02 17:32:47 +0200

    Removed tooltip from Revert button inside Full Details view

--- src/components/Navigation/Sections/HeaderSection.js
+++ src/components/Navigation/Sections/HeaderSection.js
@@ -73,12 +73,10 @@ export default class HeaderSection extends Component {
         </Col>
         <Col md={4} className="text-right">
           {!isEmpty(changes) && (
-            <ButtonWithTooltip tip="Revert all changes of the current definition">
-              <Button bsStyle="danger" data-test-id="header-section-revert-button" onClick={() => handleRevert()}>
-                <i className="fas fa-undo" />
-                <span>&nbsp;Revert Changes</span>
-              </Button>
-            </ButtonWithTooltip>
+            <Button bsStyle="danger" data-test-id="header-section-revert-button" onClick={() => handleRevert()}>
+              <i className="fas fa-undo" />
+              <span>&nbsp;Revert Changes</span>
+            </Button>
           )}{' '}
           {modalView && (
             <Button
