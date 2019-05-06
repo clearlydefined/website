b8b60f6d54c75952774548438a5d65665731822e
Simone Torrisi <storrisi@gmail.com>
2019-05-06 15:43:19 +0200

    Added CopyUrlButton to FullDetailsPage

--- src/components/Navigation/Sections/HeaderSection.js
+++ src/components/Navigation/Sections/HeaderSection.js
@@ -11,6 +11,9 @@ import Curation from '../../../utils/curation'
 import ScoreRenderer from '../Ui/ScoreRenderer'
 import DefinitionTitle from '../Ui/DefinitionTitle'
 import DefinitionRevision from '../Ui/DefinitionRevision'
+import CopyUrlButton from '../../CopyUrlButton'
+import { ROUTE_DEFINITIONS } from '../../../utils/routingConstants'
+import EntitySpec from '../../../utils/entitySpec'
 
 class HeaderSection extends Component {
   static propTypes = {
@@ -65,6 +68,12 @@ class HeaderSection extends Component {
                     Pending curations
                   </Tag>
                 )}
+              </div>
+              <div>
+                <CopyUrlButton
+                  bsStyle="info"
+                  path={`${ROUTE_DEFINITIONS}/${EntitySpec.fromObject(get(item, 'coordinates')).toPath()}`}
+                />
               </div>
             </div>
             <p>
