7162648aaa95c67f8aa7cda8d262fc10badc8e34
Simone Torrisi <storrisi@gmail.com>
2019-06-26 23:07:15 +0200

    add debian image to filter bar

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -13,6 +13,7 @@ import git from '../images/Git-Logo-2Color.png'
 import crate from '../images/cargo.png'
 import gem from '../images/gem.png'
 import pypi from '../images/pypi.png'
+import debian from '../images/debian.png'
 
 const types = {
   npm: npm,
@@ -22,7 +23,8 @@ const types = {
   nuget: nuget,
   git: git,
   crate: crate,
-  pod: pod
+  pod: pod,
+  debian: debian
 }
 
 export default class FilterBar extends Component {
