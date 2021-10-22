2c354882a645f090728f5ce3f8f5b08eaa675219
Nell Shamrell <nells@microsoft.com>
2021-10-22 14:57:10 -0700

    wip
    Signed-off-by: Nell Shamrell <nells@microsoft.com>

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'jest-puppeteer',
-  //  testRegex: 'e2e/tests/.*\\.js$',
-  testRegex: 'e2e/tests/definitions.js',
+  testRegex: 'e2e/tests/.*\\.js$',
+  //  testRegex: 'e2e/tests/definitions.js',
   globals: {
     __HOST__: process.env.TEST_HOST || 'http://localhost:3000'
   },
