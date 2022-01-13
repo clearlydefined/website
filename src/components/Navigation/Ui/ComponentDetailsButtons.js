652281ab9281d85f1862b433d00472e1fda4a26c
Nell Shamrell <nells@microsoft.com>
2022-01-13 11:57:12 -0800

    fixes build
    Signed-off-by: Nell Shamrell <nells@microsoft.com>

--- src/components/Navigation/Ui/ComponentDetailsButtons.js
+++ src/components/Navigation/Ui/ComponentDetailsButtons.js
@@ -11,132 +11,134 @@ import shareIcon from '../../../images/icons/share.svg'
 import linkCopyIcon from '../../../images/icons/linkCopy.svg'
 import { IconButton } from '@material-ui/core'
 import { asyncLocalStorage } from 'redux-persist/storages'
-constructor(props) {
-  super(props)
-  this.copyUrlButton = React.createRef()
-}
 
-isSourceComponent(component) {
-  return ['github', 'sourcearchive', 'debsrc'].includes(component.provider)
-}
-
-openSourceForComponent = definition => {
-  // const sourceLocation = get(definition, 'described.sourceLocation')
-  // const sourceEntity = sourceLocation && EntitySpec.fromObject(sourceLocation)
-  // const path = `${ROUTE_DEFINITIONS}/${EntitySpec.fromObject(sourceEntity).toPath()}`
-  const path = `/?type=${get(definition, 'coordinates.type')}&sortDesc=true&sort=releaseDate&name=${get(
-    definition,
-    'coordinates.name'
-  )}`
-  console.log('pathpathpath', path)
-  window.open(`${window.location.origin}${path}`)
-}
-
-openRelatedComponents = definition => {
-  const path = `/?type=${get(definition, 'coordinates.type')}&name=${get(
-    definition,
-    'coordinates.name'
-  )}&sortDesc=true&sort=releaseDate`
-  window.open(`${window.location.origin}${path}`)
-}
-
-renderUrl() {
-  const { item } = this.props
-  return `${window.location.origin}${ROUTE_DEFINITIONS}/${EntitySpec.fromObject(get(item, 'coordinates')).toPath()}`
-}
-
-copyToClipboard = str => {
-  const el = document.createElement('textarea')
-  el.value = str
-  el.setAttribute('readonly', '')
-  el.style.position = 'absolute'
-  el.style.left = '-9999px'
-  document.body.appendChild(el)
-  const selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false
-  el.select()
-  document.execCommand('copy')
-  document.body.removeChild(el)
-  if (selected) {
-    document.getSelection().removeAllRanges()
-    document.getSelection().addRange(selected)
+class ComponentDetailsButtons extends Component {
+  constructor(props) {
+    super(props)
+    this.copyUrlButton = React.createRef()
   }
-}
 
-openRegistryURL = item => {
-  window.open(get(item, 'described.urls.registry'))
-}
+  isSourceComponent(component) {
+    return ['github', 'sourcearchive', 'debsrc'].includes(component.provider)
+  }
 
-render() {
-  const { item } = this.props
-  const isSourceComponent = this.isSourceComponent(item.coordinates)
-  const isSourceEmpty = Definition.isSourceEmpty(item)
-  const menu = (
-    <Menu>
-      <Menu.Item key="1" onClick={() => this.copyToClipboard(this.renderUrl())}>
-        Copy component URL{' '}
-        <ButtonWithTooltip tip="Copy URL to clipboard" placement="bottom">
-          <Button className="list-fa-button">
-            <i className="fas fa-copy" />
-          </Button>
-        </ButtonWithTooltip>
-      </Menu.Item>
-      {!isSourceComponent && !isSourceEmpty && (
-        <Menu.Item key="2" onClick={() => this.openSourceForComponent(item)}>
-          Open the definition for source{' '}
-          <ButtonWithTooltip tip="Open the definition for source that matches this package" placement="bottom">
+  openSourceForComponent = definition => {
+    // const sourceLocation = get(definition, 'described.sourceLocation')
+    // const sourceEntity = sourceLocation && EntitySpec.fromObject(sourceLocation)
+    // const path = `${ROUTE_DEFINITIONS}/${EntitySpec.fromObject(sourceEntity).toPath()}`
+    const path = `/?type=${get(definition, 'coordinates.type')}&sortDesc=true&sort=releaseDate&name=${get(
+      definition,
+      'coordinates.name'
+    )}`
+    console.log('pathpathpath', path)
+    window.open(`${window.location.origin}${path}`)
+  }
+
+  openRelatedComponents = definition => {
+    const path = `/?type=${get(definition, 'coordinates.type')}&name=${get(
+      definition,
+      'coordinates.name'
+    )}&sortDesc=true&sort=releaseDate`
+    window.open(`${window.location.origin}${path}`)
+  }
+
+  renderUrl() {
+    const { item } = this.props
+    return `${window.location.origin}${ROUTE_DEFINITIONS}/${EntitySpec.fromObject(get(item, 'coordinates')).toPath()}`
+  }
+
+  copyToClipboard = str => {
+    const el = document.createElement('textarea')
+    el.value = str
+    el.setAttribute('readonly', '')
+    el.style.position = 'absolute'
+    el.style.left = '-9999px'
+    document.body.appendChild(el)
+    const selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false
+    el.select()
+    document.execCommand('copy')
+    document.body.removeChild(el)
+    if (selected) {
+      document.getSelection().removeAllRanges()
+      document.getSelection().addRange(selected)
+    }
+  }
+
+  openRegistryURL = item => {
+    window.open(get(item, 'described.urls.registry'))
+  }
+
+  render() {
+    const { item } = this.props
+    const isSourceComponent = this.isSourceComponent(item.coordinates)
+    const isSourceEmpty = Definition.isSourceEmpty(item)
+    const menu = (
+      <Menu>
+        <Menu.Item key="1" onClick={() => this.copyToClipboard(this.renderUrl())}>
+          Copy component URL{' '}
+          <ButtonWithTooltip tip="Copy URL to clipboard" placement="bottom">
             <Button className="list-fa-button">
-              <i className="fas fa-code" />
+              <i className="fas fa-copy" />
             </Button>
           </ButtonWithTooltip>
         </Menu.Item>
-      )}
-      <Menu.Item key="3" onClick={() => this.openRelatedComponents(item)}>
-        List other versions of this component{' '}
-        <Button className="list-fa-button">
-          <i class="fas fa-list" />
-        </Button>
-      </Menu.Item>
-      <Menu.Item key="4" onClick={() => this.openRegistryURL(item)}>
-        Open package URL{' '}
-        <ButtonWithTooltip tip="Open package URL" placement="bottom">
+        {!isSourceComponent && !isSourceEmpty && (
+          <Menu.Item key="2" onClick={() => this.openSourceForComponent(item)}>
+            Open the definition for source{' '}
+            <ButtonWithTooltip tip="Open the definition for source that matches this package" placement="bottom">
+              <Button className="list-fa-button">
+                <i className="fas fa-code" />
+              </Button>
+            </ButtonWithTooltip>
+          </Menu.Item>
+        )}
+        <Menu.Item key="3" onClick={() => this.openRelatedComponents(item)}>
+          List other versions of this component{' '}
           <Button className="list-fa-button">
-            <i className="fas fa-external-link-alt" />
+            <i class="fas fa-list" />
           </Button>
-        </ButtonWithTooltip>
-      </Menu.Item>
-    </Menu>
-  )
-  return (
-    <>
-      <IconButton
-        className="radius-btn border mx-2"
-        onClick={() => this.openRegistryURL(item)}
-        title="Open package URL"
-      >
-        <img src={shareIcon} alt="" />
-      </IconButton>
-      <IconButton
-        className="radius-btn border mx-2"
-        onClick={() => this.openSourceForComponent(item)}
-        title="List other versions of this component"
-      >
-        <img src={techIcon} alt="" />
-      </IconButton>
-      <IconButton
-        className="radius-btn border mx-2"
-        onClick={() => this.copyToClipboard(this.renderUrl())}
-        title="Copy component URL"
-      >
-        <img src={linkCopyIcon} alt="" />
-      </IconButton>
-    </>
-    // <Dropdown overlay={menu} trigger="hover">
-    //   <Button bsStyle="info">
-    //     <i class="fas fa-ellipsis-v" />
-    //   </Button>
-    // </Dropdown>
-  )
-}
+        </Menu.Item>
+        <Menu.Item key="4" onClick={() => this.openRegistryURL(item)}>
+          Open package URL{' '}
+          <ButtonWithTooltip tip="Open package URL" placement="bottom">
+            <Button className="list-fa-button">
+              <i className="fas fa-external-link-alt" />
+            </Button>
+          </ButtonWithTooltip>
+        </Menu.Item>
+      </Menu>
+    )
+    return (
+      <>
+        <IconButton
+          className="radius-btn border mx-2"
+          onClick={() => this.openRegistryURL(item)}
+          title="Open package URL"
+        >
+          <img src={shareIcon} alt="" />
+        </IconButton>
+        <IconButton
+          className="radius-btn border mx-2"
+          onClick={() => this.openSourceForComponent(item)}
+          title="List other versions of this component"
+        >
+          <img src={techIcon} alt="" />
+        </IconButton>
+        <IconButton
+          className="radius-btn border mx-2"
+          onClick={() => this.copyToClipboard(this.renderUrl())}
+          title="Copy component URL"
+        >
+          <img src={linkCopyIcon} alt="" />
+        </IconButton>
+      </>
+      // <Dropdown overlay={menu} trigger="hover">
+      //   <Button bsStyle="info">
+      //     <i class="fas fa-ellipsis-v" />
+      //   </Button>
+      // </Dropdown>
+    )
+  }
 }
 
 export default ComponentDetailsButtons
