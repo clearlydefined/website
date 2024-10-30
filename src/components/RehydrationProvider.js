c0ae50d0b502ecff8db9a64bba0a29f051c50ad4
Lewis Jones <ljones140@gmail.com>
2024-10-30 10:55:52 +0000

    Remove

--- src/components/RehydrationProvider.js
+++ src/components/RehydrationProvider.js
@@ -18,7 +18,6 @@ import {
   ROUTE_GETINVOLED,
   ROUTE_CHARTER,
   ROUTE_FILE,
-  ROUTE_HEALTH
 } from '../utils/routingConstants'
 import history from '../config/history'
 import { configureStore } from '../configureStore'
@@ -100,7 +99,6 @@ export default class RehydrationDelayedProvider extends Component {
               <Route path={ROUTE_STATUS} component={withTracker(PageStatus)} />
               <Route path={ROUTE_DISCORD} component={() => (window.location = 'https://discord.gg/wEzHJku')} />
               <Route path={ROUTE_FILE} component={withTracker(PageFile)} />
-              <Route path={ROUTE_HEALTH} component={withTracker(HealthCheck)} />
               <Route path={ROUTE_ROOT} component={withTracker(PageBrowse)} />
             </Switch>
           </App>
